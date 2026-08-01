dc1fe9564fca0c839c9c3a7d22d4c566619b2831
Susmitha polisetty <susmipolisetti@gmail.com>
2026-08-01 13:34:22 +0530

    Create array_basics.py

---
+++ array_basics.py
@@ -0,0 +1,8 @@
+# Array Basics
+
+numbers = [10, 20, 30, 40, 50]
+
+print("Array:", numbers)
+print("First Element:", numbers[0])
+print("Last Element:", numbers[-1])
+print("Array Length:", len(numbers))
